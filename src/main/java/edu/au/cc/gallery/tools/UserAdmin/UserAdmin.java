43b309c232e3c8356f675d86ef197dad39ea559e
Annie <anniekatherine.piotrowski@gmail.com>
2020-06-12 16:59:25 +0000

    tested webserver working.

--- src/main/java/edu/au/cc/gallery/tools/UserAdmin/UserAdmin.java
+++ src/main/java/edu/au/cc/gallery/tools/UserAdmin/UserAdmin.java
@@ -1,34 +1,23 @@
 package edu.au.cc.gallery.tools;
 import java.sql.SQLException;
 import org.json.simple.JSONObject;
-import static spark.Spark.*;
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
 import java.util.Map;
 import java.util.HashMap;
 public class UserAdmin {
     DB myDatabase;
+    UserAdminHelper myHelper;
 
     public UserAdmin() {
 	myDatabase = new DB();
+	myHelper = new UserAdminHelper();
     }
     
     public static void main(String[] args) {
 	UserAdmin admin = new UserAdmin();
+	
 	System.out.println("Hello world testing!");
-	port(5000);
-	get("/hello", (req, res) -> "Hello World");
-	get("/goodbye", (req, res) -> "Goodbye!");
-
-	get("/greet/:name", (req, res) -> "Nice to meet you " + req.params(":name"));
-	get("/add", (req, res) -> "the sum is " + (Integer.parseInt(req.queryParams("x")) + Integer.parseInt(req.queryParams("y"))));
-
-	get("/calculator", (req, res) -> {
-		Map<String, Object> model = new HashMap<String, Object>();
-		model.put("name", "Fred");
-		return new HandlebarsTemplateEngine()
-		    .render(new ModelAndView(model, "calculator.hbs"));
-	    });
+	admin.myHelper.addRoutes();
+	
 	/*try {
 	   JSONObject printOut =  admin.myDatabase.getSecret();
 	   // String password = admin.myDatabase.getPassword(printOut);
