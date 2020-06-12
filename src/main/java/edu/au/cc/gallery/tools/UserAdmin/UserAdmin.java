cbdcbab2690f4d64da284fee4b6ffdf623b7fb22
Annie <anniekatherine.piotrowski@gmail.com>
2020-06-12 13:50:33 +0000

    Reached Calculator for Fred step.

--- src/main/java/edu/au/cc/gallery/tools/UserAdmin/UserAdmin.java
+++ src/main/java/edu/au/cc/gallery/tools/UserAdmin/UserAdmin.java
@@ -1,6 +1,11 @@
 package edu.au.cc.gallery.tools;
 import java.sql.SQLException;
 import org.json.simple.JSONObject;
+import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+import java.util.Map;
+import java.util.HashMap;
 public class UserAdmin {
     DB myDatabase;
 
@@ -11,9 +16,20 @@ public class UserAdmin {
     public static void main(String[] args) {
 	UserAdmin admin = new UserAdmin();
 	System.out.println("Hello world testing!");
-	
+	port(5000);
+	get("/hello", (req, res) -> "Hello World");
+	get("/goodbye", (req, res) -> "Goodbye!");
 
-	try {
+	get("/greet/:name", (req, res) -> "Nice to meet you " + req.params(":name"));
+	get("/add", (req, res) -> "the sum is " + (Integer.parseInt(req.queryParams("x")) + Integer.parseInt(req.queryParams("y"))));
+
+	get("/calculator", (req, res) -> {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("name", "Fred");
+		return new HandlebarsTemplateEngine()
+		    .render(new ModelAndView(model, "calculator.hbs"));
+	    });
+	/*try {
 	   JSONObject printOut =  admin.myDatabase.getSecret();
 	   // String password = admin.myDatabase.getPassword(printOut);
 	   //String user = admin.myDatabase.getUserName(printOut);
@@ -31,7 +47,7 @@ public class UserAdmin {
 	}
 	catch (SQLException ex) {
 	    ex.printStackTrace();
-	    } 
+	    } */
 
 	        
     }
