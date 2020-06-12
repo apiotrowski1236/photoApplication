f73b2a5ffcff427493b72b0c6bbf9990aea301f1
Annie <anniekatherine.piotrowski@gmail.com>
2020-06-12 12:05:20 +0000

    Retrieving select * from

--- src/main/java/edu/au/cc/gallery/tools/UserAdmin/UserAdmin.java
+++ src/main/java/edu/au/cc/gallery/tools/UserAdmin/UserAdmin.java
@@ -1,5 +1,6 @@
 package edu.au.cc.gallery.tools;
 import java.sql.SQLException;
+import org.json.simple.JSONObject;
 public class UserAdmin {
     DB myDatabase;
 
@@ -9,12 +10,29 @@ public class UserAdmin {
     
     public static void main(String[] args) {
 	UserAdmin admin = new UserAdmin();
+	System.out.println("Hello world testing!");
+	
+
 	try {
+	   JSONObject printOut =  admin.myDatabase.getSecret();
+	   // String password = admin.myDatabase.getPassword(printOut);
+	   //String user = admin.myDatabase.getUserName(printOut);
+	   System.out.println(printOut);
+	   //  System.out.println(user);
+	}
+	catch(Exception e) {
+	    System.out.println("failed to get secret!");
+	    e.printStackTrace();
+	}
+		try {
 	    admin.myDatabase.connect();
+	    admin.myDatabase.listUsers();
+	    admin.myDatabase.close();
 	}
 	catch (SQLException ex) {
 	    ex.printStackTrace();
-	}
-	System.out.println("Hello world testing!");
+	    } 
+
+	        
     }
 }
