8e8da74a3140f10bfe5fd0ed3679f3aec6957641
EC2 Default User <ec2-user@ip-10-0-4-127.us-east-2.compute.internal>
2020-06-29 14:22:17 +0000

    Added structure for photo-specific implementation of the DAO interface.

--- src/main/java/edu/au/cc/gallery/tools/UserAdmin/data/PhotoDAO.java
+++ src/main/java/edu/au/cc/gallery/tools/UserAdmin/data/PhotoDAO.java
@@ -1,6 +1,25 @@
+import java.util.List;
+import java.util.ArrayList;
 public class PhotoDAO implements DAO<Photo> {
+    private List<Photo> photos = new ArrayList<>();
+
+    public PhotoDAO() {
+
+    }
+
+    public List<Photo> getAll() {
+	return photos;
+    }
 
 
+    public void add(Photo photo) {
+
+    }
+
+    public void delete(Photo photo) {
+
+    }
+  
 
 
 
